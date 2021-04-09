1bbc5e5d91fc7d5ac84dd142f397f47d66423290
Raymund Rafael <raymund.adrian.rafael@gmail.com>
2021-04-09 10:53:39 +0800

    Initial App with redux and eslint

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import styled from '@emotion/styled';
+
+import ControlBar from './components/ControlBar';
 import './App.css';
 
+const Container = styled('div')({ width: '100%' });
+
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <ControlBar />
+    </Container>
   );
 }
 
---
+++ src/components/ControlBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Container = styled('div')({
+  width: '100%',
+  backgroundColor: '#EFF3F6',
+  height: 59,
+  padding: '13px 17px 12px 25px'
+});
+
+const ControlBar = () => {
+
+  return (
+    <Container>
+      <div>test</div>
+    </Container>
+  );
+};
+
+export default ControlBar;
